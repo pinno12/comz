d792a142e04a0c20b4b6c54b8ba6658612e52f55
wonny <pinno12@kaist.ac.kr>
2021-04-06 07:55:02 +0900

    comz first update

---
+++ public/script.js
@@ -0,0 +1,22 @@
+$('.ui.modal')
+  .modal('show')
+;
+
+$('.tool-tip').popup();
+
+
+$('.ui.checkbox')
+  .checkbox()
+  ;
+
+  $('.menu .item')
+  .tab()
+;
+
+$('.accordion')
+  .accordion({
+    selector: {
+      trigger: '.title'
+    }
+  })
+;
